740ff66c8c70b4d9126fec5225da577c19543b0c
JamesLinz <jlalencar@gmail.com>
2021-12-07 14:18:53 -0300

    Projeto Criado

---
+++ src/main/java/com/project/dio_personalapi/DioPersonalapiApplication.java
@@ -0,0 +1,13 @@
+package com.project.dio_personalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioPersonalapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioPersonalapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/dio_personalapi/DioPersonalapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dio_personalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioPersonalapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
